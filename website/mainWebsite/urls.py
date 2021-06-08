6b0491f0e5be3902c95d2fd6312c09103e7da0e0
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2021-06-08 15:40:05 -0400

    added html pages

--- website/mainWebsite/urls.py
+++ website/mainWebsite/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="site-home"),
+    path("buy", views.Buy, name="buy"),
+    path("sell", views.Sell, name="sell"),
+    path("login", views.Login, name="login"),
+    path("register", views.Register, name="register"),
+    path("account", views.Account, name="account"),
+    path("yourscripts", views.YourScripts, name="yourscripts"),
+    path("settings", views.Settings, name="settings"),
 ]
--- website/mainWebsite/views.py
+++ website/mainWebsite/views.py
@@ -2,5 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'mainWebsite/index.html')
-#123
+    return render(request, 'mainWebsite/layout.html')
+
+def Buy(request):
+    return render(request, 'mainWebsite/buy.html')
+
+def Sell(request):
+    return render(request, 'mainWebsite/sell.html')
+
+def Login(request):
+    return render(request, 'mainWebsite/login.html')
+
+def Register(request):
+    return render(request, 'mainWebsite/register.html')
+
+
+def Account(request):
+    return render(request, 'mainWebsite/register.html')
+
+
+def YourScripts(request):
+    return render(request, 'mainWebsite/yourscripts.html')
+
+def Settings(request):
+    return render(request, 'mainWebsite/settings.html')
