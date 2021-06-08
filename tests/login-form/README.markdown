d336bdbfe0d69ddc1b40a58d57bc08c44fcaa307
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2021-06-08 16:22:19 -0400

    random

---
+++ tests/login-form/README.markdown
@@ -0,0 +1,5 @@
+# LogIn Form
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/nedev/pen/EVaGqW](https://codepen.io/nedev/pen/EVaGqW).
+
+
--- website/mainWebsite/views.py
+++ website/mainWebsite/views.py
@@ -2,27 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'mainWebsite/layout.html')
+    return render(request, 'mainWebsite/layout.html', {'title': 'Main'})
 
 def Buy(request):
-    return render(request, 'mainWebsite/buy.html')
+    return render(request, 'mainWebsite/buy.html', {'title': 'Buy'})
 
 def Sell(request):
-    return render(request, 'mainWebsite/sell.html')
+    return render(request, 'mainWebsite/sell.html', {'title': 'Sell'})
 
 def Login(request):
-    return render(request, 'mainWebsite/login.html')
+    return render(request, 'mainWebsite/login.html', {'title': 'Login'})
 
 def Register(request):
-    return render(request, 'mainWebsite/register.html')
+    return render(request, 'mainWebsite/register.html', {'title': 'Register'})
 
 
 def Account(request):
-    return render(request, 'mainWebsite/register.html')
+    return render(request, 'mainWebsite/register.html', {'title': 'Account'})
 
 
 def YourScripts(request):
-    return render(request, 'mainWebsite/yourscripts.html')
+    return render(request, 'mainWebsite/yourscripts.html', {'title': 'Scripts'})
 
 def Settings(request):
-    return render(request, 'mainWebsite/settings.html')
+    return render(request, 'mainWebsite/settings.html', {'title': 'Settings'})
